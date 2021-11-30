40f15a519dcdd671cfca97470a2ab33fad6ed3da
Pavel Hardziyevich <pashagordievich@gmail.com>
2021-11-30 23:11:54 +0300

    changed ParserFactory. Added correct validation.

--- src/main/java/by/hardziyevich/task2/factory/ParserFactory.java
+++ src/main/java/by/hardziyevich/task2/factory/ParserFactory.java
@@ -4,28 +4,26 @@ import by.hardziyevich.task2.exeption.SomeException;
 import by.hardziyevich.task2.parser.CandyParser;
 import by.hardziyevich.task2.parser.CandySAXParser;
 import by.hardziyevich.task2.parser.CandyStAXParser;
+import by.hardziyevich.task2.validator.Validator;
 
 public class ParserFactory {
 
-    private ParserFactory(){}
+    private ParserFactory() {
+    }
 
-    public enum TypeParser{
+    public enum TypeParser {
         StAX,
         SAX
     }
 
     public static CandyParser newCandyParser(TypeParser type) throws SomeException {
-        if(type != null) {
-            switch (type) {
-                case SAX:
-                    return new CandySAXParser();
-                case StAX:
-                    return new CandyStAXParser();
-                default:
-                    throw new SomeException("This type is not supported");
-            }
-        }else {
-            throw new SomeException("This type is not supported");
+        switch (Validator.of(type).get()) {
+            case SAX:
+                return new CandySAXParser();
+            case StAX:
+                return new CandyStAXParser();
+            default:
+                throw new SomeException("This type is not supported");
         }
     }
 }
